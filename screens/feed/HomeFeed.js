a567974ba8b9d2ebc330fdea7c634af8a669d8e7
Sadeed Ahmad <sadeedahmedkhan.official@gmail.com>
2022-09-21 14:31:09 +0500

    banner done

--- screens/feed/HomeFeed.js
+++ screens/feed/HomeFeed.js
@@ -1,8 +1,29 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button, Icon } from '@rneui/themed';
+import { View, Text, ScrollView, FlatList, StyleSheet } from 'react-native';
+import { Button, Icon, Image } from '@rneui/themed';
 import InputField from '../../components/InputField';
+import { FlatListSlider } from 'react-native-flatlist-slider';
 import COLORS from '../../constants/colors';
+
+const images = [
+  {
+    banner: require('../../assets/images/offer-banner.png'),
+    desc: 'First image',
+  },
+  {
+    banner: require('../../assets/images/offer-banner.png'),
+    desc: 'Second image',
+  },
+  {
+    banner: require('../../assets/images/offer-banner.png'),
+  },
+  {
+    banner: require('../../assets/images/offer-banner.png'),
+  },
+  {
+    banner: require('../../assets/images/offer-banner.png'),
+  },
+];
 
 const HomeFeed = (props) => {
   const { navigation } = props;
@@ -53,22 +74,80 @@ const HomeFeed = (props) => {
   });
 
   return (
-    <View style={styles.container}>
-      <Button
-        title={'go to sale'}
-        onPress={() => {
-          props.navigation.navigate('sale');
+    <ScrollView contentContainerStyle={styles.container}>
+      <View
+        style={{
+          height: 250,
+          // backgroundColor: 'pink',
+          width: '90%',
+          marginTop: 10,
+          borderRadius: 5,
         }}
-      />
-    </View>
+      >
+        <FlatListSlider
+          data={images}
+          imageKey={'banner'}
+          width={343}
+          height={206}
+          separatorWidth={15}
+          local
+          loop={false}
+          autoscroll={false}
+          indicatorActiveColor={COLORS.blue}
+          indicatorInActiveColor={COLORS.grey}
+        />
+
+        {/* <FlatList
+          data={[, , , , ,]}
+          renderItem={({ item }) => (
+            <Image
+              source={require('../../assets/images/offer-banner.png')}
+              containerStyle={{
+                aspectRatio: 1,
+                height: 206,
+                width: 343,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        /> */}
+      </View>
+      <View
+        style={{
+          height: 200,
+          backgroundColor: 'grey',
+          width: '90%',
+          marginTop: 10,
+        }}
+      ></View>
+      <View
+        style={{
+          height: 200,
+          backgroundColor: 'lightblue',
+          width: '90%',
+          marginTop: 10,
+        }}
+      ></View>
+      <View
+        style={{
+          height: 200,
+          backgroundColor: 'lightgreen',
+          width: '90%',
+          marginTop: 10,
+        }}
+      ></View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     backgroundColor: COLORS.white,
   },
   header: {
